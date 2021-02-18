3b6cf000a2716b6d70c981f863e1757a5ff36d33
David Beauchesne <david.beauchesne@uqar.ca>
2021-02-18 12:24:32 -0500

    package skeleton and data

---
+++ R/ais.R
@@ -0,0 +1,78 @@
+#' Create navigation tracks from AIS data.
+#'
+#' A function to create navigation tracks from AIS data based on maximum distance and maximum time interval between observations.
+#'
+#' @param ais csv of AIS positional data
+#' @param time_interval maximum time interval allowed between points, in hours
+#' @param distance maximum distance allowed between points, in meters
+#' @keywords AIS
+#' @keywords navigation
+#'
+#' @export
+#'
+#' @details This function takes basic `.csv` files containing AIS observation data and creates line segments divided by individual boats (`MMSI` field) and by a user defined maximum time internal and distance between observations.
+#'
+#'
+#' @examples
+#' # Example 1:
+#' ais <- ais_segment(df)
+
+ais_segment <- function(df, time_interval = (300/60), distance = (50 * 1.61 * 1000)) {
+  # =~-~=~-~=~-~=~-~=~-~=~-~= #
+  # CSV AS SF POINTS DATA
+  # =~-~=~-~=~-~=~-~=~-~=~-~= #
+  ais <- df %>%
+         st_as_sf(coords = c("Longitude", "Latitude"), crs = 4326) %>%
+         st_transform(32198)
+
+
+  # =~-~=~-~=~-~=~-~=~-~=~-~= #
+  # TIME INTERALS
+  # =~-~=~-~=~-~=~-~=~-~=~-~= #
+  # Create `POSIXct` column from `Date_UTC`
+  # The AIS format for date and time: `20051224_163958`
+  # The format has to be transformed to the form: `2005-12-24 16:39:58`
+  yr <- substr(ais$Date_UTC, 1, 4)
+  mt <- substr(ais$Date_UTC, 5, 6)
+  dy <- substr(ais$Date_UTC, 7, 8)
+  hr <- substr(ais$Date_UTC, 10, 11)
+  mn <- substr(ais$Date_UTC, 12, 13)
+  sc <- substr(ais$Date_UTC, 14, 15)
+  POSIXct <- paste0(yr, '-', mt, '-', dy, ' ', hr, ':', mn, ':', sc)
+
+  # Add time to AIS data
+  ais$POSIXct <- as.POSIXct(POSIXct)
+
+  # Order data by ship and date/time
+  ais <- arrange(ais, MMSI, POSIXct)
+
+  # Time interval
+  ais$time_int <- c(0,difftime(ais$POSIXct[-1], ais$POSIXct[-nrow(ais)], units = "hours"))
+
+
+  # =~-~=~-~=~-~=~-~=~-~=~-~= #
+  # DISTANCE
+  # =~-~=~-~=~-~=~-~=~-~=~-~= #
+  # Distance between points
+  ais$distance_int <- c(0,st_distance(ais[-1, ], ais[-nrow(ais), ], by_element = TRUE))
+
+
+
+  # =~-~=~-~=~-~=~-~=~-~=~-~= #
+  # SEGMENTS
+  # =~-~=~-~=~-~=~-~=~-~=~-~= #
+  # Identify individual tracks according to `time_interval` and `distance` selected
+  ais <- ais %>%
+         mutate(time_group = ifelse(ais$time_int > time_interval, TRUE, FALSE),
+                distance_group = ifelse(ais$distance_int > distance, TRUE, FALSE),
+                group = time_group + distance_group) %>%
+         mutate(group = cumsum(group)+1)
+
+  # Create line segments
+  ais %>%
+  group_by(MMSI, group) %>%
+  arrange(POSIXct) %>%
+  summarize(n = n(), .groups = "drop", do_union = FALSE) %>% # do_union = FALSE is important to preserve order
+  filter(n > 2) %>%
+  st_cast("LINESTRING")
+}
---
+++ scripts/getData.R
@@ -0,0 +1,85 @@
+# =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+# Notes
+# ---------------------------------------
+#
+# =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+# This script is built to get the data from various sources on the internet and
+# locally
+#
+# The package is not meant for publication, and this script is also not meant
+# to be reprpducible
+#
+# The goal is to facilitate the analyses and remove the need to load the data
+# from various repos all the time
+#
+# The actual repos should be fully reproducible, and the final workflow should
+# not call data from the package
+#
+# This is mostly for exploration purposes and ease of work in the future
+#
+# Use to see list of data available in the package:
+# data(package = "ceanavR")
+# _____________________________________________________________________________ #
+
+
+# =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+# Libraries
+# ---------------------------------------
+#
+# =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+library(sf)
+library(stars)
+library(tidyverse)
+library(magrittr)
+# _____________________________________________________________________________ #
+
+
+# =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+# Study area
+# ---------------------------------------
+#
+# =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+studyarea <- 'https://github.com/EffetsCumulatifsNavigation/ZoneEtude/raw/main/Data/StudyArea/StudyArea.geojson'
+download.file(studyarea, destfile = './data/StudyArea.geojson')
+studyarea <- st_read('./data/StudyArea.geojson')
+save(studyarea, file = './data/studyarea.RData')
+file.remove('./data/StudyArea.geojson')
+# _____________________________________________________________________________ #
+
+
+
+# =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+# Study grids
+# ---------------------------------------
+#
+# =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+# URLs
+grid1000poly <- 'https://github.com/EffetsCumulatifsNavigation/ZoneEtude/raw/main/Data/StudyGrid/Grid_Poly1000.geojson'
+grid2000poly <- 'https://github.com/EffetsCumulatifsNavigation/ZoneEtude/raw/main/Data/StudyGrid/Grid_Poly2000.geojson'
+grid1000ras <- 'https://github.com/EffetsCumulatifsNavigation/ZoneEtude/raw/main/Data/StudyGrid/Grid_Raster1000.gpkg'
+grid2000ras <- 'https://github.com/EffetsCumulatifsNavigation/ZoneEtude/raw/main/Data/StudyGrid/Grid_Raster2000.gpkg'
+
+# Download
+download.file(grid1000poly, destfile = './data/Grid_Poly1000.geojson')
+download.file(grid2000poly, destfile = './data/Grid_Poly2000.geojson')
+download.file(grid1000ras, destfile = './data/Grid_Raster1000.gpkg')
+download.file(grid2000ras, destfile = './data/Grid_Raster2000.gpkg')
+
+# Import
+grid1000poly <- st_read('./data/Grid_Poly1000.geojson')
+grid2000poly <- st_read('./data/Grid_Poly2000.geojson')
+grid1000ras <- read_stars('./data/Grid_Raster1000.gpkg')
+grid2000ras <- read_stars('./data/Grid_Raster2000.gpkg')
+
+# Export
+save(grid1000poly, file = './data/grid1000poly.RData')
+save(grid2000poly, file = './data/grid2000poly.RData')
+save(grid1000ras, file = './data/grid1000ras.RData')
+save(grid2000ras, file = './data/grid2000ras.RData')
+
+# Delete
+file.remove('./data/Grid_Poly1000.geojson')
+file.remove('./data/Grid_Poly2000.geojson')
+file.remove('./data/Grid_Raster1000.gpkg')
+file.remove('./data/Grid_Raster2000.gpkg')
+# _____________________________________________________________________________ #
