e0f23667f771efb6a3cfb1ae0614206ff76d2e1f
David Beauchesne <david.beauchesne@uqar.ca>
2021-06-11 16:38:09 -0400

    add contacts and work on marshes, but hard to say which data to use, warning to check

--- R/cv_habitat.R
+++ R/cv_habitat.R
@@ -24,5 +24,124 @@ cv_habitats <- function() {
   # Add info to grid
   zostere <- aoi %>% mutate(zostere = 0)
   zostere$zostere[zostereID] <- 1
-  
+
+  # # Combine datasets
+  # milieu_humide <- bind_rows(milieu_humide_lsp, milieu_humide_stl)
+  #
+  # # Load grid
+  # data(aoi_grid1000poly)
+  #
+  # # Identify grid cells with zostera
+  # uid <- st_intersects(milieu_humide, aoi) %>%
+  #              unlist() %>%
+  #              unique()
+  #
+  # # Add info to grid
+  # milieu_humide <- aoi %>% mutate(milieu_humide = 0)
+  # milieu_humide$milieu_humide[uid] <- 1
+
+  # Format milieu humide db
+  fmtMarais <- function () {
+
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    # Load data
+    # ------------------------------------
+    #
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    folder <- './analysis/data/cv/habitats/marais/'
+
+    # dataID: 0005
+    # Marais Saint-Laurent
+    marais_stl <- st_read(paste0(folder, 'marais_saint-laurent/MaraisCotiers_Publ_janv2019/MaraisCotiers_Publ_janv2019.shp')) %>%
+                         st_transform(32198)
+    # ------------------------------------------------------------------------- #
+
+
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    # Format data
+    # ------------------------------------
+    #
+    # All we will do for now for this dataset is include it as presence-absence
+    # in the study grid
+    #
+    # So I intersect the milieu humide db with the grid to identify which grid
+    # cell intersect with the db
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    # Load grid
+    data(aoi_grid1000poly)
+
+    # Identify grid cells with zostera
+    uid <- st_intersects(marais_stl, aoi) %>%
+                 unlist() %>%
+                 unique()
+
+    # Add info to grid
+    marais <- aoi %>% mutate(marais = 0)
+    marais$marais[uid] <- 1
+    # ------------------------------------------------------------------------- #
+
+
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    # Export data
+    # ------------------------------------
+    #
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    save(marais, file = './data/cv_hab_marais.RData')
+    # ------------------------------------------------------------------------- #
+
+  }
+
+
+  # Format milieu humide db
+  fmtMilieu_sableux <- function () {
+
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    # Load data
+    # ------------------------------------
+    #
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    folder <- './analysis/data/cv/habitats/milieu_sableux/'
+
+    # dataID: 0005
+    # Marais Saint-Laurent
+    milieu_sableux_stl <- st_read(paste0(folder, 'milieu_sableux_saint-laurent/MilieuxSableux_Publ_janv2019/MilieuxSableux_Publ_janv2019.shp')) %>%
+                          st_transform(32198)
+    # ------------------------------------------------------------------------- #
+
+
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    # Format data
+    # ------------------------------------
+    #
+    # All we will do for now for this dataset is include it as presence-absence
+    # in the study grid
+    #
+    # So I intersect the milieu humide db with the grid to identify which grid
+    # cell intersect with the db
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    # Load grid
+    data(aoi_grid1000poly)
+
+    # Identify grid cells with zostera
+    uid <- st_intersects(milieu_sableux_stl, aoi) %>%
+                 unlist() %>%
+                 unique()
+
+    # Add info to grid
+    milieu_sableux <- aoi %>% mutate(milieu_sableux = 0)
+    milieu_sableux$milieu_sableux[uid] <- 1
+    # ------------------------------------------------------------------------- #
+
+
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    # Export data
+    # ------------------------------------
+    #
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    save(milieu_sableux, file = './data/cv_hab_milieu_sableux.RData')
+    # ------------------------------------------------------------------------- #
+
+  }
+
+
 }
--- R/data0005-milieu_cotier.R
+++ R/data0005-milieu_cotier.R
@@ -1,14 +1,12 @@
-#' Data 0005 : Milieux côtiers Saint-Laurent
+#' Data 0005 : Milieux humides cartographie détaillée
 #'
-#' Atlas des milieux côtiers d’intérêt pour la conservation dans l’estuaire et # le golfe du Saint-Laurent
+#' Cartographie détaillée des milieux humides pour les basses-terres du Saint-Laurent, la plaine du Lac Saint-Jean et d'autres secteurs au sud du Québec
 #'
 #' @keywords milieu humide
-#' @keywords marais côtier
-#' @keywords côte sableuse
 #' @keywords habitat
 #' @keywords composante valorisée
 #'
-#' @source https://catalogue.ogsl.ca/fr/dataset/0a232214-05cc-438a-b914-6a8b53ac184e
+#' @source https://www.donneesquebec.ca/recherche/dataset/eafec419-d67d-449e-a157-d22230314d36
 #'
 #' @export
 #'
@@ -20,32 +18,55 @@ get_data0005 <- function() {
   # Download data
   # ----------------------------------------
   # Output folder
-  output <- "data0005-milieu_cotier/"
+  output <- "data0005-milieu_humide/"
   folder <- paste0("./data/data-raw/", output)
   if (!file.exists(folder)) dir.create(folder)
 
   # Proceed only if data is not already loaded
-  if (!file.exists(paste0(folder, 'atlas_estuairegolfe_rapport_final_fr.pdf'))) {
+  if (!file.exists(paste0(folder, 'metadonneesmh.pdf'))) {
     # URL
-    milieu_humide_stl <- c('https://catalogue.ogsl.ca/data/pasl/0a232214-05cc-438a-b914-6a8b53ac184e/atlas_estuairegolfe_rapport_final_fr.pdf',
-                           'https://catalogue.ogsl.ca/data/pasl/0a232214-05cc-438a-b914-6a8b53ac184e/milieuxhum_publ_janv2019.zip')
+    data0005 <- c('https://www.donneesquebec.ca/recherche/dataset/eafec419-d67d-449e-a157-d22230314d36/resource/089ac31a-4b49-41e0-b334-2525b1dcfb66/download/mhsudduqc2020.zip',
+                  'https://www.donneesquebec.ca/recherche/dataset/eafec419-d67d-449e-a157-d22230314d36/resource/a4d4078a-005f-4eda-904a-f98c635ee4f9/download/classesdemilieuxhumides.pdf',
+                  'https://www.donneesquebec.ca/recherche/dataset/eafec419-d67d-449e-a157-d22230314d36/resource/36622527-c0f5-4af1-9e3a-aa149d311b7f/download/metadonneesmh.pdf')
 
     # Download
-    download.file(milieu_humide_stl[1], destfile = paste0(folder, 'atlas_estuairegolfe_rapport_final_fr.pdf'))
-    download.file(milieu_humide_stl[2], destfile = paste0(folder, 'milieuxhum_publ_janv2019.zip'))
+    download.file(data0005[1], destfile = paste0(folder, 'mhsudduqc2020.zip'))
+    download.file(data0005[2], destfile = paste0(folder, 'classesdemilieuxhumides.pdf'))
+    download.file(data0005[3], destfile = paste0(folder, 'metadonneesmh.pdf'))
+
 
     # Unzip
-    unzip(zipfile = paste0(folder, 'milieuxhum_publ_janv2019.zip'), exdir = folder)
-  }  # _________________________________________________________________________ #
-
-  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
-  # Import data
-  # ----------------------------------------
-  # Milieux humides lac Saint-Pierre
-  data0005 <- st_read(paste0(folder, 'MilieuxHum_Publ_janv2019/MilieuxHum_Publ_janv2019.shp'))
-
+    unzip(zipfile = paste0(folder, 'mhsudduqc2020.zip'), exdir = folder)
+  }
   # _________________________________________________________________________ #
 
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  # Import and format data
+  # ----------------------------------------
+  data0005 <- st_read(paste0(folder, 'MH_sudduQC_2020.shp'))
+
+  # WARNING:
+  # "The dataset is very big and covers a much broader area than what we need
+  # for this project. I will therefore only select polygons that intersect with
+  # our study area. Simply rqemove the code that intersects with our study area
+  # to get the whole dataset"
+  message("The dataset is very big and covers a much broader area than what we need for this project.
+  I will therefore only select polygons that intersect with our study area. Simply remove the code that
+  intersects with our study area to get the whole dataset")
+
+  # Study area grid
+  data(aoi_grid1000poly)
+  aoi <- st_transform(aoi, st_crs(data0005))
+
+  # Identify polygons intersecting study area
+  uid <- st_intersects(aoi, data0005) %>%
+         unlist() %>%
+         unique() %>%
+         sort()
+
+  # Select polygons intersecting study area
+  data0005 <- data0005[uid, ]
+  # _________________________________________________________________________ #
 
   # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
   # Export data
