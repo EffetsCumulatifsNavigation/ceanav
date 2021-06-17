89bf93e0113f0cff289ec82d48c2f20efa77982a
David Beauchesne <david.beauchesne@uqar.ca>
2021-06-17 16:19:32 -0400

    peche melecite

---
+++ R/data0025-wolastoqiyik_wahsipekuk.R
@@ -0,0 +1,53 @@
+#' Data 0025 : Communauté Wolastoqiyik Wahsipekuk pêche homard
+#'
+#' Localisation des sites d'importance pour la pêche aux homards dans l'estuaire du Saint-Laurent pour l'année 2019
+#'
+#' @keywords homard Américain
+#' @keywords Wolastoqiyik Wahsipekuk
+#' @keywords sites d'importance
+#' @keywords composante valorisée
+#'
+#' @source Première Nation Wolastoqiyik Wahsipekuk (2021) Localisation des sites d'importance pour la pêche aux homards dans l'estuaire du Saint-Laurent pour l'année 2019.
+#'
+#' @export
+#'
+#' @details Cette fonction formatte les données
+#'
+
+get_data0025 <- function() {
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  # Download data
+  # ----------------------------------------
+  # WARNING:
+  message("Les données brutes sont soumis à une entente de partage de données et doivent être supprimées au terme du projet")
+
+  # Output folder
+  output <- "data0025-wolastoqiyik_wahsipekuk/"
+  folder <- paste0("./data/data-raw/", output)
+  if (!file.exists(folder)) dir.create(folder)
+
+  # WARNING: Data transfered physically, no cloud access currently
+  # _________________________________________________________________________ #
+
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  # Import and format data
+  # ----------------------------------------
+  # WARNING: Retirer information sur les débarquements et uniquement utiliser la
+  #          délimitation des sites pêchés
+  data0025 <- st_read(paste0(folder, "Debarquement_homard.shp")) %>%
+              select(-Debar.lbs.)
+  # _________________________________________________________________________ #
+
+
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  # Export data
+  # ----------------------------------------
+  # Output
+  st_write(obj = data0025,
+           dsn = "./data/data-format/data0025-wolastoqiyik_wahsipekuk.geojson",
+           delete_dsn = TRUE)
+
+  # RData
+  save(data0025, file = "./data/data0025.RData")
+  # _________________________________________________________________________ #
+}
---
+++ R/data0026-wolastoqiyik_wahsipekuk.R
@@ -0,0 +1,63 @@
+#' Data 0026 : Communauté Wolastoqiyik Wahsipekuk pêche oursin vert
+#'
+#' Localisation des sites d'importance pour la pêche à l'oursin vert dans l'estuaire du Saint-Laurent.
+#'
+#' @keywords oursin vert
+#' @keywords Wolastoqiyik Wahsipekuk
+#' @keywords sites d'importance
+#' @keywords composante valorisée
+#'
+#' @source Première Nation Wolastoqiyik Wahsipekuk (2021) Localisation des sites d'importance pour la pêche à l'oursin vert dans l'estuaire du Saint-Laurent
+#'
+#' @export
+#'
+#' @details Cette fonction formatte les données
+#'
+
+get_data0026 <- function() {
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  # Download data
+  # ----------------------------------------
+  # WARNING:
+  message("Les données brutes sont soumis à une entente de partage de données et doivent être supprimées au terme du projet")
+
+  # Output folder
+  output <- "data0026-wolastoqiyik_wahsipekuk/"
+  folder <- paste0("./data/data-raw/", output)
+  if (!file.exists(folder)) dir.create(folder)
+
+  # WARNING: Data transfered physically, no cloud access currently
+  # _________________________________________________________________________ #
+
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  # Import and format data
+  # ----------------------------------------
+  # WARNING: Retirer information sur les débarquements et uniquement utiliser la
+  #          délimitation des sites pêchés
+  data0026 <- list()
+  data0026[[1]] <- st_read(paste0(folder, "Oursins_zones_exploitation_potentielleA.shp"))
+  data0026[[2]] <- st_read(paste0(folder, "Oursins_zones_exploitation_potentielleB.shp"))
+  data0026[[3]] <- st_read(paste0(folder, "Oursins_zones_exploitation_potentielleC.shp"))
+
+  # Add missing id in dataset
+  data0026[[1]]$id <- "A"
+  data0026[[2]]$id <- "B"
+  data0026[[3]]$id <- "C"
+
+  # Single dataset
+  data0026 <- bind_rows(data0026)
+  # _________________________________________________________________________ #
+
+
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  # Export data
+  # ----------------------------------------
+  # Output
+  st_write(obj = data0026,
+           dsn = "./data/data-format/data0026-wolastoqiyik_wahsipekuk.geojson",
+           delete_dsn = TRUE)
+
+  # RData
+  save(data0026, file = "./data/data0026.RData")
+  # _________________________________________________________________________ #
+}
