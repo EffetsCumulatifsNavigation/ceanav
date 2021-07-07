12cac30ecd76f4d33e3abca9bd371acf15c09213
David Beauchesne <david.beauchesne@uqar.ca>
2021-07-07 16:11:44 -0400

    remove hyperlink function from export

--- R/zzz.R
+++ R/zzz.R
@@ -1,10 +1,11 @@
 # Internals
-#' @importFrom fs path path_package 
+#' @importFrom fs path path_package
 #' @importFrom glue glue glue_sql
 #' @importFrom whisker whisker.render
 #' @importFrom yaml yaml.load_file write_yaml read_yaml
 NULL
 
+# ------------------------------------------------------------------------------
 # Gracieuseté de Kevin Cazelles: https://github.com/KevCaz
 # my simple(r) version of use template
 use_template <- function(template, save_as = stdout(), pkg = "ceanav", ...) {
@@ -13,4 +14,22 @@ use_template <- function(template, save_as = stdout(), pkg = "ceanav", ...) {
   )
   # NB by default whisker forward the parent envi and I used this
   writeLines(whisker::whisker.render(template, ...), save_as)
+}
+
+# ------------------------------------------------------------------------------
+# Création d'un hyperlien en format markdown à partir de deux vecteurs
+rep_hyperlien <- function(texte, url) {
+  nl <- length(texte)
+  hyperlien <- character(nl)
+
+  for(i in 1:nl) {
+    if(!is.na(url[i])) {
+      hyperlien[i] <- paste0("[",texte[i],"](",url[i],")")
+    } else {
+      hyperlien[i] <- texte[i]
+    }
+  }
+
+  # Return
+  hyperlien
 }
