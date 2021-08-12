5ac44bbac9655c543064298a7e7345d4adf14398
David Beauchesne <david.beauchesne@uqar.ca>
2021-08-12 13:48:02 -0400

    aire protegees quebec

--- R/00-pipeline.R
+++ R/00-pipeline.R
@@ -77,6 +77,7 @@ pipeline <- function(
     get_data0035()
     get_data0036()
     get_data0037()
+    get_data0038()
   }
 
 
---
+++ R/data0038-aires_protogees.R
@@ -0,0 +1,58 @@
+#' Data 0040 : Registre des aires protégées au Québec
+#'
+#' Ces thématiques présentent la compilation des aires protégées au Québec. 
+#'
+#' @keywords
+#' @keywords
+#' @keywords
+#'
+#' @source https://www.donneesquebec.ca/recherche/dataset/aires-protegees-au-quebec
+#'
+#' @export
+#'
+#' @details This function loads and formats the data
+#'
+
+get_data0038 <- function() {
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  # Download data
+  # ----------------------------------------
+  # Output folder
+  output <- "data0038-aire_protegee/"
+  folder <- paste0("./data/data-raw/", output)
+  if (!file.exists(folder)) dir.create(folder)
+
+  # Proceed only if data is not already loaded
+  if (!file.exists(paste0(folder, 'registre_aires_prot.zip'))) {
+    # URL
+    dat <- c('ftp://ftp.mddelcc.gouv.qc.ca/DONNEES_OUVERTES/Registre_aires_protegees/registre_aires_prot.zip',
+             'ftp://ftp.mddelcc.gouv.qc.ca/DONNEES_OUVERTES/Registre_aires_protegees/AP_structures_donnees.pdf',
+             'ftp://ftp.mddelcc.gouv.qc.ca/DONNEES_OUVERTES/Registre_aires_protegees/LISEZ_MOI.pdf')
+
+    # Download
+    download.file(dat[1], destfile = paste0(folder, 'registre_aires_prot.zip'))
+    download.file(dat[2], destfile = paste0(folder, 'AP_structures_donnees.pdf'))
+    download.file(dat[2], destfile = paste0(folder, 'LISEZ_MOI.pdf'))
+
+    # Unzip
+    unzip(zipfile = paste0(folder, 'registre_aires_prot.zip'), exdir = folder)
+  }
+  # _________________________________________________________________________ #
+
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  # Import data
+  # ----------------------------------------
+  data0038 <- st_read(paste0(folder, 'registre_aires_prot/AP_REG_S.shp'), quiet = TRUE) %>%
+              st_transform(crs = global_parameters()$crs)
+  # _________________________________________________________________________ #
+
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  # Export data
+  # ----------------------------------------
+  # Output
+  st_write(obj = data0038,
+           dsn = "./data/data-format/data0038-aire_protegee.geojson",
+           delete_dsn = TRUE)
+  # _________________________________________________________________________ #
+
+}
