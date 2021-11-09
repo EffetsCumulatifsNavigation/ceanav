fc77cc2e52a6173862c1436abd44a65c0a4285de
David Beauchesne <david.beauchesne@uqar.ca>
2021-11-08 22:37:19 -0500

    cdpnq faune

---
+++ R/data0059-cdpnq_faune.R
@@ -0,0 +1,50 @@
+#' Data 0059 : Espèces fauniques à statut
+#'
+#' Présence d'espèces fauniques menacées ou vulnérables ou susceptibles d'être ainsi désignées ou rares
+#'
+#' @keywords faune
+#' @keywords espèce à statut
+#' @keywords habitat
+#' @keywords composante valorisée
+#'
+#' @source
+#'
+#' @export
+#'
+#' @details Cette fonction formatte les données
+#'
+
+get_data0059 <- function() {
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  # Download data
+  # ----------------------------------------
+  # Output folder
+  output <- "data0059-cdpnq_faune/"
+  folder <- paste0("./data/data-raw/", output)
+  if (!file.exists(folder)) dir.create(folder)
+
+  # WARNING: Data transfered physically, no cloud access currently
+
+  # Unzip
+  unzip(zipfile = paste0(folder, 'CDPNQ_Faune_fleuve400m.zip'), exdir = folder)
+
+  # _________________________________________________________________________ #
+
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  # Import data
+  # ----------------------------------------
+  data0059 <- st_read(paste0(folder, 'CDPNQ_Faune_fleuve400m.shp'), quiet = TRUE) %>%
+              st_transform(crs = global_parameters()$crs)
+  # _________________________________________________________________________ #
+
+
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  # Export data
+  # ----------------------------------------
+  # Output
+  st_write(obj = data0059,
+           dsn = "./data/data-format/data0059-cdpnq_faune.geojson",
+           delete_dsn = TRUE,
+           quiet = TRUE)
+  # _________________________________________________________________________ #
+}
