3e657684eb093cb635244bb6817ff587be4121d0
David Beauchesne <david.beauchesne@uqar.ca>
2021-12-01 19:11:39 -0500

    vulnerability matrix mammiferes marins

---
+++ R/vuln_mammiferes_marins.R
@@ -0,0 +1,86 @@
+#' Vulnérabilité des mammifères marins aux stresseurs environnementaux
+#'
+#' Création de la matrice de vulnérabilité pour les mammifères marins considérés pour l'évaluation des effets cumulatifs
+#'
+#' @keywords vulnérabilité
+#' @keywords mammifères marins
+#' @keywords composante valorisée
+#'
+#' @export
+#'
+#' @details Cette fonction génère la matrice de vulnérabilité pour les habitats
+#'
+
+vuln_mammiferes_marins <- function() {
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  # Data import
+  # ------------------------------------
+  #
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  # Vulnerability matrix
+  load_format("data0064")
+
+  # ------------------------------------------------------------------------- #
+
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  # Vulnerability scores
+  # ------------------------------------
+  #
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  uid <- !colnames(data0064) %in% c("categories","cv")
+  data0064$vulnerabilite <- rowSums(data0064[, uid])
+
+  # Normalize
+  data0064$vulnerabilite <- data0064$vulnerabilite / max(data0064$vulnerabilite)
+
+  # Long to wide
+  data0064 <- pivot_wider(data0064, id_cols = categories, names_from = cv, values_from = vulnerabilite)
+
+  # As matrix
+  uid <- colnames(data0064) == "categories"
+  vulnerability_mammiferes_marins <- as.matrix(data0064[, !uid])
+  rownames(vulnerability_mammiferes_marins) <- data0064$categories
+  # ------------------------------------------------------------------------- #
+
+
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  # Adjust vulnerability scores
+  # ----------------------------------
+  #
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  # Diminution de 10% pour les déversements de types autres
+  vulnerability_mammiferes_marins["autres", ] <- vulnerability_mammiferes_marins["autres", ]*.9
+
+  # Round everything
+  vulnerability_mammiferes_marins <- round(vulnerability_mammiferes_marins, 2)
+  # _____________________________________________________________________________ #
+
+
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  # Update metadata
+  # ----------------------------------
+  #
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+
+
+  # _____________________________________________________________________________ #
+
+
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  # Export data
+  # ------------------------------------
+  #
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  # -----
+  write.csv(vulnerability_mammiferes_marins,
+            "./data/data-vulnerability/vulnerability_mammiferes_marins.csv",
+            row.names = TRUE)
+  # ------------------------------------------------------------------------- #}
+
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  # Clean global environment
+  #
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  clean()
+  # ------------------------------------------------------------------------- #}
+}
