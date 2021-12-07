1ea68a24ff22ecdf15d8b46b9c992277aebdd73e
David Beauchesne <david.beauchesne@uqar.ca>
2021-12-07 15:40:28 -0500

    add regional summary

--- R/ana_cumulative_effects_region_km2.R
+++ R/ana_cumulative_effects_region_km2.R
@@ -19,41 +19,79 @@ ana_cumulative_effects_region_km2 <- function() {
   # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
   # -----
   load_format("data0065")
+  data(grid1p)
+
+  # -----
+  # Identify which regions intersect with which grid cells
+  uid <- list()
+  for(i in 1:nrow(data0065)) {
+    uid[[i]] <- st_intersects(data0065[i,], grid1p) %>%
+    unlist()
+  }
+
+  # -----
+  # Identify which regions intersect with any grid cell
+  iid <- logical(length(uid))
+  for(i in 1:nrow(data0065)) iid[i] <- length(uid[[i]]) > 10
+
+  # -----
+  # Keep only regions with data
+  data0065 <- data0065[iid, ]
+  uid <- uid[iid]
+
   load_output("composantes_valorisees_format")
   load_output("stresseurs_format")
   cv <- st_drop_geometry(composantes_valorisees_format)
   st <- st_drop_geometry(stresseurs_format)
 
   # -----
-  cekm <- matrix(ncol = ncol(st)+3, nrow = ncol(cv), data = 0,
-                 dimnames = list(c(), c("cv","area","cea",colnames(st)))) %>%
+  cekm <- matrix(ncol = ncol(st)+3, nrow = nrow(data0065), data = 0,
+                 dimnames = list(c(), c("region","area","cea",colnames(st)))) %>%
           data.frame()
 
   # -----
-  cekm$cv <- colnames(cv)
+  cekm$region <- data0065$RES_NM_REG
 
   # -----
-  cekm$area <- colSums(cv, na.rm = TRUE)
+  for(i in 1:length(uid)) cekm$area[i] <- length(uid[[i]])
 
   # -----
-  # Now get metrics for each valued component
-  folder <- "data/data-output/cea_composante_valorisee/"
+  # Now get metrics for each stressor
+  folder <- "data/data-output/cea_stresseur/"
   files <- dir(folder)
-  cvNames <- gsub("cea_", "", files) %>% gsub(".csv","",.)
+  stNames <- gsub("cea_", "", files) %>% gsub(".csv","",.)
   for(i in 1:length(files)) {
     # ---
-    uid <- cekm$cv == cvNames[i]
+    dat <- read.csv(glue("{folder}{files[i]}"))
 
     # ---
-    dat <- read.csv(glue("{folder}{files[i]}")) %>%
-           colSums(na.rm = TRUE) / cekm$area[uid]
-
-    # ---
-    cekm[uid, names(dat)] <- round(dat, 6)
-
-    # ---
-    cekm$cea[uid] <- round(sum(dat), 6)
+    # Iterate over regions
+    # TODO: Remove na.rm = TRUE
+    for(j in 1:nrow(cekm)) cekm[j, stNames[i]] <- sum(dat[uid[[j]], ], na.rm = TRUE) / cekm$area[j]
   }
+
+  # ---
+  for(i in 1:nrow(cekm)) cekm$cea[i] <- round(sum(cekm[i, stNames]), 6)
+
+  # # -----
+  # # Now get metrics for each valued component
+  # folder <- "data/data-output/cea_composante_valorisee/"
+  # files <- dir(folder)
+  # cvNames <- gsub("cea_", "", files) %>% gsub(".csv","",.)
+  # for(i in 1:length(files)) {
+  #   # ---
+  #   uid <- cekm$cv == cvNames[i]
+  #
+  #   # ---
+  #   dat <- read.csv(glue("{folder}{files[i]}")) %>%
+  #          colSums(na.rm = TRUE) / cekm$area[uid]
+  #
+  #   # ---
+  #   cekm[uid, names(dat)] <- round(dat, 6)
+  #
+  #   # ---
+  #   cekm$cea[uid] <- round(sum(dat), 6)
+  # }
 
   # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
   # Export
