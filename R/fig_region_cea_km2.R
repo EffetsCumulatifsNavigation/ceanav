a1efbece30bc672225e87e75ba44d7a0b1cdcdea
David Beauchesne <david.beauchesne@uqar.ca>
2021-12-08 16:57:19 -0500

    update region figures

--- R/fig_region_cea_km2.R
+++ R/fig_region_cea_km2.R
@@ -65,14 +65,49 @@ st_ellide_rotate = function(x, degrees, center_coords=NULL){
   return((x-center_coords) * transform_matrix + center_coords)
 }
 
-center_coords <- sf::st_combine(x) %>% sf::st_centroid()
+center_coords <- sf::st_combine(regions) %>% sf::st_centroid()
 l <- list()
 for(i in 1:nrow(regions)) {
   l[[i]] <- st_ellide_rotate(regions[i, ], 35, center_coords) %>%
             st_sf() %>%
             mutate(cea = regions$cea[i])
 }
-regions <- bind_rows(l)
+regions_rotate <- bind_rows(l)
+
+# Region centroids
+regions_center <- st_centroid(regions) %>%
+                  st_ellide_rotate(35, center_coords)
+
+# Normalize spatial objects
+# regions_rotate <- st_normalize(regions_rotate)
+# regions_center <- st_normalize(regions_center, domain = st_bbox(regions_rotate))
+
+#=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=#
+# Lines between graph 1 and 2
+#=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=#
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# NOTE: This is true of the current settings from 2nd graph only
+#       Should be updated if x and y ranges change for either graph
+g1 <- c(-543864.9, 182186.7)
+g2 <- c(-3.56, 11.56)
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# Graph ranges
+g1_range <- diff(g1)
+g2_range <- diff(g2)
+
+# Relative position on 2nd graph
+vertical_lines <- 1:nrow(cekm)
+vertical_lines_relative <- (vertical_lines - g2[1]) / g2_range
+
+# Transform that to relative positions on 1st graph
+vertical_lines_1 <- (vertical_lines_relative * g1_range) +g1[1]
+
+# XY1 graph 1
+xy1 <- st_coordinates(regions_center)
+
+# Lines coordinates
+l <- data.frame(x1 = xy1[,'X'], y1 = xy1[,'Y'], x2 = vertical_lines_1, y2 = 385000)
+
 
 #=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=#
 # Colors
@@ -105,20 +140,20 @@ pal <- colorRampPalette(viridis::viridis(100))
 # Stresseurs
 #=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=#
 # trans1 <- c('FF','DD','BB','99','77','55','33','11')
-trans2 <- c('FF','DD','AA','88','66','44')
-trans3 <- c('FF','DD','BB','99','77','55','33','11','22','44','66','88','AA','CC','EE')
+# trans2 <- c('FF','DD','AA','88','66','44')
+# trans3 <- c('FF','DD','BB','99','77','55','33','11','22','44','66','88','AA','CC','EE')
 st$col <- gg_color_hue(nGroup)[as.numeric(as.factor(st$title))]
 # for(i in 1:nrow(st)) st$col[i] <- darken(st$col[i], 20)
 
-for(i in levels(as.factor(st$title))) {
-  id <- which(st$title == i)
-  nId <- length(id)
-  if (nId > 5) trans <- trans3
-  if (nId <= 5) trans <- trans2
-  for(j in 1:nId) {
-    st$col[id[j]] <- paste0(substr(st$col[id[j]],1,7), trans[j])
-  }
-}
+# for(i in levels(as.factor(st$title))) {
+#   id <- which(st$title == i)
+#   nId <- length(id)
+#   if (nId > 5) trans <- trans3
+#   if (nId <= 5) trans <- trans2
+#   for(j in 1:nId) {
+#     st$col[id[j]] <- paste0(substr(st$col[id[j]],1,7), trans[j])
+#   }
+# }
 
 #=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=#
 # Graph data
@@ -141,21 +176,54 @@ dat <- cekm[, c("region", st$fullname)] %>%
 #=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=#
 png('./figures/figures-output/cumulative_effects_region_cea.png', res = 300, width = 300, height = 300, units = "mm")
 layout(matrix(1:2, 2, 1), heights = c(.25,.75))
-par(mar = c(0,3,0,3))
-cols <- regions$cea
-cols <- pal(101)[((regions$cea / max(regions$cea))*100)+1]
 
-plot(st_geometry(regions), col = cols)
-# plot(st_geometry(aoi), add = TRUE)
+#=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=#
+# Map
+#=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=#
+par(mar = c(0,2,0,2))
+cols <- regions_rotate$cea
+cols <- pal(101)[((regions_rotate$cea / max(regions_rotate$cea))*100)+1]
+# plot0(c(0,1), c(0,1))
+# plot(st_geometry(regions_rotate), col = cols, add = TRUE)
+plot(st_geometry(regions_rotate), col = cols)
+plot(st_geometry(regions_center), add = TRUE, pch = 20, col = "#909090", cex = 1.5)
+# points(x = vertical_lines_1, y = rep(385000, length(vertical_lines_1)), cex = 2,  col = 'red')
+for(i in 1:nrow(l)) lines(x = c(l$x1[i],l$x2[i]), y = c(l$y1[i],l$y2[i]), lty = 2, col = "#909090", lwd = 1.5)
 
+# Legend
+maxDat <- max(regions_rotate$cea)
+cols <- pal(101)[((regions_rotate$cea / maxDat)*100)+1]
+plot_legend_cont(
+  range = range(regions_rotate$cea),
+  pal = pal,
+)
 
+bbox <- st_bbox(regions_rotate)
+text(x = bbox$xmin + 1000,
+     y = bbox$ymax,
+     labels = "Effets cumulatifs par région administrative",
+     font = 2,
+     adj = c(0,.5),
+     cex = 1
+   )
+
+text(x = bbox$xmin + 2500,
+     y = bbox$ymax - 10000,
+     labels = TeX("Effets cumulatifs * $km^{-2}"),
+     font = 2,
+     adj = c(0,.5),
+     cex = .7
+   )
+#=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=#
+# Poiint graph
+#=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=#
 par(family = 'serif')
-par(mar = c(0,0,0,0))
+par(mar = c(0,2,0,2))
 xR <- c(-3, nrow(cekm))
 yR <- c(-5, nrow(st))
 plot0(x = xR, y = yR)
-for(i in 1:nrow(cekm)) lines(x = c(i,i), y = c(1,yR[2]), lty = 3, col = "#00000044")
-# for(i in 1:nrow(st)) lines(x = c(1,xR[2]), y = c(i,i), lty = 3, col = "#00000044")
+for(i in 1:nrow(cekm)) lines(x = c(i,i), y = c(1,yR[2]), lty = 2, lwd = 1.5, col = "#909090")
+# for(i in 1:nrow(st)) lines(x = c(1,xR[2]), y = c(i,i), lty = 2, lwd = 1.5, col = "#909090")
 points(x = dat$rg_id, y = dat$st_id, cex = dat$cea*1.5, col = "#000000", bg = dat$col, pch = 21)
 text(x = 1:xR[2], y = 0, labels = cekm$region, srt = 45, cex = .85, adj = c(1,.5))
 text(x = .5, y = 1:yR[2], labels = st$francais, cex = .85, adj = c(1,.5))
