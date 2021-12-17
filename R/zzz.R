b4af083dc41f75c3d9a9c8163688fff52e9f372a
David Beauchesne <david.beauchesne@uqar.ca>
2021-12-17 10:01:14 -0500

    add tables for vulnerability berge and habitat

--- R/zzz.R
+++ R/zzz.R
@@ -2,7 +2,7 @@
 #' @importFrom exactextractr exact_extract
 #' @importFrom fs path path_package
 #' @importFrom glue glue glue_sql
-#' @importFrom kableExtra kable_styling row_spec
+#' @importFrom kableExtra kable_styling row_spec column_spec
 #' @importFrom latex2exp TeX
 #' @importFrom magick image_read image_append image_write
 #' @importFrom raster getData
