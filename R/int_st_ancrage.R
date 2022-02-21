dcf5515c48f611f489ea75f25b2150f6e17caa0a
David Beauchesne <david.beauchesne@uqar.ca>
2022-02-21 11:20:20 -0500

    update ancrage with new data

--- R/int_st_ancrage.R
+++ R/int_st_ancrage.R
@@ -17,22 +17,46 @@ st_ancrage <- function() {
   #
   # WARNING: Method has to be revisited
   # https://github.com/EffetsCumulatifsNavigation/ceanav/issues/6
+  # 
+  # After discussions with Corinne Berthold, the plan is to: 
+  #   - Take all of the GCC dataset (0070)
+  #   - Take the SHC dataset (0071) beyond the GCC dataset, i.e. east of Les Rasades
   #
   # For now, I will do something simple:
-  #   1. 2km buffer around anchorages - arbitrary
+  #   1. 1km buffer around anchorages - arbitrary
   #   2. Intersect with grid
   #   3. Number of buffers intersecting grid cell
   # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
   # Load data & metadata
-  load_format("data0015")
+  load_format("data0070")
+  load_format("data0071")
   meta <- read_yaml("./data/data-metadata/int_st_ancrage.yml")
-  meta$rawData <-  c("0015")
+  meta$rawData <-  c("0070","0071")
 
   # Load grid
   data(grid1p)
+  
+  # -----
+  # Select data overlapping with aoi for both datasets 
+  # data0070
+  uid <- st_intersects(grid1p, data0070) %>% unlist() %>% unique() %>% sort()
+  data0070 <- data0070[uid, ]
+  
+  # data0071
+  uid <- st_intersects(grid1p, data0071) %>% unlist() %>% unique() %>% sort()
+  data0071 <- data0071[uid, ]
 
   # -----
-  ancrage <- st_buffer(data0015, 2000) %>%
+  # Data east of Les Rasades for data0071 
+  xy <- st_centroid(data0070) %>% st_coordinates() 
+  maxLong <- max(xy[,"X"])
+  xy <- st_centroid(data0071) %>% st_coordinates()
+  uid <- xy[,"X"] > maxLong
+  data0071 <- data0071[uid,]
+  
+  # -----
+  ancrage <- bind_rows(data0070, data0071) %>%
+             st_buffer(2000) %>%
              st_intersects(grid1p,.) %>%
              lapply(., length) %>%
              unlist()
@@ -53,19 +77,20 @@ st_ancrage <- function() {
   # ----------------------------------
   #
   # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
-  meta$dataDescription$spatial$extent <- st_bbox(data0015)
+  meta$dataDescription$spatial$extent <- st_bbox(bind_rows(data0070,data0071))
 
   # -----
   meta$dataDescription$categories$accronyme <-  "ancrage"
-  meta$dataDescription$categories$francais <-  "Sites d'ancrage de navires"
+  meta$dataDescription$categories$francais <-  "Sites d'ancrage commerciaux"
   meta$dataDescription$categories$source <-  meta$rawData
-  meta$dataDescription$categories$description <- "Distribution des sites d'ancrage de navires" 
+  meta$dataDescription$categories$description <- "Distribution des sites d'ancrage pour la navigation commerciale" 
 
   # --- For proper referencing in markdown syntax
   meta$dataDescription$categories$mdref <- modif_md(meta$dataDescription$categories$accronyme)
 
   # -----
-  meta$dataDescription$observations$total <-  st_intersects(data0015, aoi) %>%
+  meta$dataDescription$observations$total <-  bind_rows(data0070,data0071) %>%
+                                              st_intersects(aoi) %>%
                                               unlist() %>%
                                               length()
   # ------------------------------------------------------------------------- #
