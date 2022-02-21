98dada85cf01909375e54e19e4564a5486ad30b4
David Beauchesne <david.beauchesne@uqar.ca>
2022-02-21 12:48:28 -0500

    update naufrages

--- R/00-pipeline.R
+++ R/00-pipeline.R
@@ -223,11 +223,11 @@ pipeline <- function(
     # Données intégrées (portrait)
     # Stresseurs
     folder <- "report/contenu/2-portrait/1-stresseurs/"
-    rep_portrait_data_description(data_id = "ancrage", output_folder = folder)
+    # rep_portrait_data_description(data_id = "ancrage", output_folder = folder)
     # rep_portrait_data_description(data_id = "deversement", output_folder = folder)
-    rep_portrait_data_description(data_id = "dragage", output_folder = folder)
-    rep_portrait_data_description(data_id = "naufrage", output_folder = folder)
-    rep_portrait_data_description(data_id = "navigation", output_folder = folder)
+    # rep_portrait_data_description(data_id = "dragage", output_folder = folder)
+    # rep_portrait_data_description(data_id = "naufrage", output_folder = folder)
+    # rep_portrait_data_description(data_id = "navigation", output_folder = folder)
     rep_portrait_data_description(data_id = "peche_commerciale", output_folder = folder)
     rep_portrait_data_description(data_id = "pollution_maritime", output_folder = folder)
 
