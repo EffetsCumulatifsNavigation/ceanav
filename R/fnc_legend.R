335dfe8927903204b16b0532776ec1d375d6469d
David Beauchesne <david.beauchesne@uqar.ca>
2022-03-01 18:10:36 -0500

    modify legend to include 0 and NA

--- R/fnc_legend.R
+++ R/fnc_legend.R
@@ -14,6 +14,7 @@
 #' @param type character, type of legend. Choices are 'continuous', 'binary' or 'categorical'
 #' @param nTick numeric, number of ticks in the legend
 #' @param minUp numeric, minimum upper side to write as a function of bbox extent
+#' @param showNA logical, add box that shows regions identified as NA in the figure
 #'
 #' @return Opens a graphical interface with the plot
 #'
@@ -25,7 +26,8 @@ plot_legend_cont <- function (range = c(0,1),
                               minUp = .175,
                               mainTitle = NULL,
                               subTitle = NULL,
-                              n = 5) {
+                              n = 5,
+                              showNA = FALSE) {
   # Legends
   # Palette
   if(class(pal) == 'character') {
@@ -47,7 +49,6 @@ plot_legend_cont <- function (range = c(0,1),
   ybarUp <- yinit - ygap/2 - .0041*yR
   ybarDn <- yinit - ygap -ygap/2 + .0041*yR
 
-
   # Plot
    x <- seq(from = xinit, to = xinit + .17*xR, by = .0003*xR)
    z <- data.frame(y1 = ybarUp,
@@ -68,12 +69,43 @@ plot_legend_cont <- function (range = c(0,1),
    lines(x = c(xinit, xinit + .17*xR), y = rep(z$y2[1], 2))
    for(i in 1:n) lines(x = rep(x[i],2), y = c(z$y2[1], z$y2[1]- .003*yR))
 
+   # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+   # 0 values as transparent
+   g0 <- .010*xR 
+   g1 <- .040*xR
+   nz <- nrow(z)
+   polygon(x = c(z$x2[nz]+g0,z$x2[nz]+g1,z$x2[nz]+g1,z$x2[nz]+g0,z$x2[nz]+g0),
+           y = c(z$y1[k],z$y1[k],z$y2[k],z$y2[k],z$y1[k]),
+           col = "#00000000",
+           border = "#000000", 
+           lwd = .5)
+   text(labels = "0", x = mean(c(z$x2[nz]+g0,z$x2[nz]+g1)), y = mean(c(z$y1, z$y2)), adj = c(.5,.5), cex = cexSub*.75)
+   # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  
+   # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+   # NA values for regions without data
+   if (showNA) {
+     g0 <- .050*xR 
+     g1 <- .080*xR
+     nz <- nrow(z)
+     polygon(x = c(z$x2[nz]+g0,z$x2[nz]+g1,z$x2[nz]+g1,z$x2[nz]+g0,z$x2[nz]+g0),
+             y = c(z$y1[k],z$y1[k],z$y2[k],z$y2[k],z$y1[k]),
+             col = global_param$col$naValues,
+             border = "#000000", 
+             lwd = .5)
+     text(labels = "NA", x = mean(c(z$x2[nz]+g0,z$x2[nz]+g1)), y = mean(c(z$y1, z$y2)), adj = c(.5,.5), cex = cexSub*.75)        
+   }
+   # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
    # Labels
    if(range[2] <= 5) {
      lab <- round(seq(from = 0, to = max(range[2]), length.out = n), 2)     
    } else {
      lab <- round(seq(from = 0, to = max(range[2]), length.out = n))     
    }
+   
+   # Add that it's > 0, not 0
+   lab[1] <- glue("> {lab[1]}")
    
    text(x = x,
         y =  rep(z$y2[1] - .01*yR, n),
@@ -115,7 +147,8 @@ plot_legend_bin <- function (col,
                              cexSub = .75,
                              minUp = .175,
                              mainTitle = NULL,
-                             subTitle = NULL) {
+                             subTitle = NULL,
+                             showNA = FALSE) {
 
   # Determine plot boundaries, in units of the data
   xmin <- par("usr")[1]
@@ -139,6 +172,20 @@ plot_legend_bin <- function (col,
           y = c(ybarUp,ybarDn,ybarDn,ybarUp,ybarUp),
           col = col,
           border = "#000000")
+
+  # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  # NA values for regions without data
+  if (showNA) {
+    g0 <- .090*xR 
+    polygon(x = c(xinit+g0,xinit+g0+sq*xR,xinit+g0+sq*xR,xinit+g0,xinit+g0),
+            y = c(ybarUp,ybarUp,ybarDn,ybarDn,ybarUp),
+            col = global_param$col$naValues,
+            border = "#000000")
+    text(labels = "NA", x = mean(c(xinit+g0,xinit+g0+sq*xR)), y = mean(c(ybarUp,ybarDn)), adj = c(.5,.5), cex = cexSub*.75)        
+  }
+  # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
 
   # Add titles
   yText <- ybarUp + .035*yR
